05211578c1327df8f058633ca4aa6dda9b70e688
Your NameSneakymax <maximusekeh@gmail.com>
2019-09-17 10:51:37 +0100

    chore(setup test): setup test environment
    - install jest and enzyme
    - configure circleci
    - refactor codebase
    
    [Finishes #168532287]

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import App from './App';
-import reducer from './store/reducer';
+import reducer from './redux/reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
